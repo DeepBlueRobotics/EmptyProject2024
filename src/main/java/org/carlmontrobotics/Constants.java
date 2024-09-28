e0a5b83b19826fcc5a1cef794b05680ea0cf1d02
Matthew Lum <144396869+Rand0mAsianKid@users.noreply.github.com>
2024-09-28 05:31:58 +0000

    Coded outline for MA drivetrain and possible solution to gradle?

--- src/main/java/org/carlmontrobotics/Constants.java
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -1,15 +1,19 @@
 package org.carlmontrobotics;
 
 public final class Constants {
-    // public static final class Drivetrain {
-    //     public static final double MAX_SPEED_MPS = 2;
-    // }
-    public static final class OI {
-        public static final class Driver {
-            public static final int port = 0;
-        }
-        public static final class Manipulator {
-            public static final int port = 1;
-        }
+
+    public final class Drivetrain{
+        public static final int RIGHT_PORT = 0;
+        public static final int LEFT_PORT = 1;
     }
+
+
+    //controllers
+    public static final class Driver {
+        public static final int DRIVER_CONTROLLER_PORT = 2;
+    }
+    public static final class Manipulator {
+        public static final int MANIPULATOR_CONTROLLER_PORT = 3;
+    }
+
 }
--- src/main/java/org/carlmontrobotics/RobotContainer.java
+++ src/main/java/org/carlmontrobotics/RobotContainer.java
@@ -7,10 +7,14 @@ package org.carlmontrobotics;
 //199 files
 // import org.carlmontrobotics.subsystems.*;
 // import org.carlmontrobotics.commands.*;
-import static org.carlmontrobotics.Constants.OI;
+
+import org.carlmontrobotics.Constants.*;
+import org.carlmontrobotics.commands.Autonomous;
 
 //controllers
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Axis;
 
 //commands
@@ -24,69 +28,45 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import org.carlmontrobotics.subsystems.Drivetrain;
+
 
 public class RobotContainer {
 
   //1. using GenericHID allows us to use different kinds of controllers
   //2. Use absolute paths from constants to reduce confusion
-  public final GenericHID driverController = new GenericHID(OI.Driver.port);
-  public final GenericHID manipulatorController = new GenericHID(OI.Manipulator.port);
+  private final XboxController driverController = new XboxController(Constants.Driver.DRIVER_CONTROLLER_PORT);
+  private final XboxController manipulatorController = new XboxController(Constants.Manipulator.MANIPULATOR_CONTROLLER_PORT);
+  private final Drivetrain drivetrain = new Drivetrain(driverController);
 
   public RobotContainer() {
-
     setDefaultCommands();
     setBindingsDriver();
     setBindingsManipulator();
   }
 
   private void setDefaultCommands() {
-    // drivetrain.setDefaultCommand(new TeleopDrive(
-    //   drivetrain,
-    //   () -> ProcessedAxisValue(driverController, Axis.kLeftY)),
-    //   () -> ProcessedAxisValue(driverController, Axis.kLeftX)),
-    //   () -> ProcessedAxisValue(driverController, Axis.kRightX)),
-    //   () -> driverController.getRawButton(OI.Driver.slowDriveButton)
-    // ));
+
   }
-  private void setBindingsDriver() {}
-  private void setBindingsManipulator() {}
+  private void setBindingsDriver() {
+
+  }
+  private void setBindingsManipulator() {
+    
+  }
+
+
+
+  public double rightJoystickValue() {
+    return driverController.getRightX();
+  }
+  public double leftJoystickValue() {
+    return driverController.getLeftY();
+  }
+
+
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
-
-  /**
-   * Flips an axis' Y coordinates upside down, but only if the select axis is a joystick axis
-   * 
-   * @param hid The controller/plane joystick the axis is on
-   * @param axis The processed axis
-   * @return The processed value.
-   */
-  private double getStickValue(GenericHID hid, Axis axis) {
-    return hid.getRawAxis(axis.value) * (axis == Axis.kLeftY || axis == Axis.kRightY ? -1 : 1);
-  }
-  /**
-   * Processes an input from the joystick into a value between -1 and 1, sinusoidally instead of linearly
-   * 
-   * @param value The value to be processed.
-   * @return The processed value.
-   */
-  private double inputProcessing(double value) {
-    double processedInput;
-    // processedInput =
-    // (((1-Math.cos(value*Math.PI))/2)*((1-Math.cos(value*Math.PI))/2))*(value/Math.abs(value));
-    processedInput = Math.copySign(((1 - Math.cos(value * Math.PI)) / 2) * ((1 - Math.cos(value * Math.PI)) / 2),
-        value);
-    return processedInput;
-  }
-  /**
-   * Combines both getStickValue and inputProcessing into a single function for processing joystick outputs
-   * 
-   * @param hid The controller/plane joystick the axis is on
-   * @param axis The processed axis
-   * @return The processed value.
-   */
-  private double ProcessedAxisValue(GenericHID hid, Axis axis){
-    return inputProcessing(getStickValue(hid, axis));
+    return new Autonomous(drivetrain);
   }
 }
